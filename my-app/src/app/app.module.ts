b892c272aa026a02d04d00d6a00f221a4d6ac106
loulou <loulou.pei@hotmail.com>
2019-01-13 22:38:44 +0100

    font and image works

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { SubsectionComponent } from './subsection/subsection.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    SubsectionComponent
+    SubsectionComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
